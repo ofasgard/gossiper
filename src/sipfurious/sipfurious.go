7780275dda200cbf823d98ed9b979346372499c8
Callum Murphy <callum.a.murphy.77@gmail.com>
2021-08-22 23:00:46 +0100

    basic udp/tcp connection functionality doesn't need any changing

--- src/sipfurious/sipfurious.go
+++ src/sipfurious/sipfurious.go
@@ -10,8 +10,17 @@ func main() {
 	sender := siplib.NewSIPRecipient("sipfabulous", "2000", "192.168.1.220", 5060)
 	receiver := siplib.NewSIPRecipient("sipferrous", "user2", "server.com", 5060)
 	
-	x := siplib.NewOptionsRequest("UDP", sender, receiver)
-	fmt.Println(x.Generate())
+	req := siplib.NewOptionsRequest("UDP", sender, receiver)
+	
+	conn,err := siplib.ConnectUDP("192.168.1.8", 5060)
+	fmt.Println(err)
+	err = siplib.SendUDP(conn, req)
+	fmt.Println(err)
+	res,err := siplib.RecvUDP(conn)
+	fmt.Println(err)
+	
+	fmt.Println(res)
+	
 }
 
 // http://siptutorial.net/SIP/request.html
