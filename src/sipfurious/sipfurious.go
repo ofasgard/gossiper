b512c189ad8d6074d36b13ce0c3110514835d62d
Callum Murphy <callum.a.murphy.77@gmail.com>
2021-08-23 17:53:25 +0100

    add first iteration of multithreaded scanner

--- src/sipfurious/sipfurious.go
+++ src/sipfurious/sipfurious.go
@@ -3,7 +3,7 @@ package main
 import "siplib"
 import "time"
 import "math/rand"
-import "fmt"
+//import "fmt"
 
 // For SIP implementation info see:
 // http://siptutorial.net/SIP/request.html
@@ -16,38 +16,7 @@ func main() {
 func simpletest() {
 	rand.Seed(time.Now().UnixNano())
 	
-	sender := siplib.NewSIPRecipient("sipfurious", "2000", "1.3.3.7", 5060)
-	receiver := siplib.NewSIPRecipient("Joe Jones", "user2", "192.168.1.8", 5060)
-	res,err := siplib.MapOptionsUDP("192.168.1.8", 5060, 10, sender, receiver)
+	siplib.ScanOptionsUDP([]string{"192.168.1.8"}, 5060, 10, 5)
 	
-	fmt.Println(res)
-	fmt.Println(err)
-}
 
-func map_udp(targets []string, port int, timeout int) {
-	res_targets := []string{}
-	results := []string{}
-	for _,target := range targets {
-		fmt.Printf("Trying %s:%d...\n", target, port)
-		result,err := siplib.MapUDP(target, port, timeout)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Could not map %s:%d (%s)\n", target, port, err.Error())
-		} else {
-			res_targets = append(res_targets, target)
-			results = append(results, result)
-		}
-	}
-	fmt.Println("")
-	if len(res_targets) > 0 {
-		w := new(tabwriter.Writer)
-		w.Init(os.Stdout, 0, 8, 2, '\t', 0)
-		fmt.Fprintf(w, "Target\tPort\tServer Header\n")
-		fmt.Fprintf(w, "\t\t\t\n")
-		for index,_ := range res_targets {
-			fmt.Fprintf(w, "%s\t%d\t%s\n", res_targets[index], port, results[index])
-		}
-		w.Flush()
-	} else {
-		fmt.Println("No results found.")
-	}
 }
--- src/siplib/map.go
+++ src/siplib/map.go
@@ -1,5 +1,7 @@
 package siplib
 
+import "fmt"
+
 // Single-threaded OPTIONS scan via UDP.
 
 func MapOptionsUDP(target string, port int, timeout int, sender SIPRecipient, receiver SIPRecipient) (string,error) {
@@ -32,4 +34,67 @@ func MapOptionsTCP(target string, port int, timeout int, sender SIPRecipient, re
 		return val,nil
 	}
 	return "[NONE]",nil
+}
+
+// Multi-threaded OPTIONS scan via UDP.
+
+func ScanOptionsUDP(targets []string, port int, timeout int, threads int) {
+	// Anonymous function for workers
+	worker := func(input chan string, output chan string, port int, timeout int) {
+		jobs := []string{}
+		//Get input from the channel until it is closed.
+		for {
+			val,ok := <-input
+			if !ok { break }
+			jobs = append(jobs, val)
+		}
+		//Perform an options scan on each target; send results to the output channel.
+		for _,job := range jobs {
+			sender := NewSIPRecipient("sipfurious", "100", "1.1.1.1", 5060)
+			receiver := NewSIPRecipient("sipfurious", "200", job, port)
+			res,err := MapOptionsUDP(job, port, timeout, sender, receiver)
+			if err == nil {
+				output <- res
+			}
+		}
+		//Close the output channel.
+		close(output)
+	}
+	
+	//Build a worker pool equal to the desired thread limit.
+	input_channels := []chan string{}
+	output_channels := []chan string{}
+	for i := 0; i<threads; i++ {
+		input := make(chan string, 0)
+		output := make(chan string, 0)
+		go worker(input, output, port, timeout)
+		input_channels = append(input_channels, input)
+		output_channels = append(output_channels, output)
+	}
+	
+	//Allocate jobs to the workers.
+	current_worker := 0
+	for i := 0; i<len(targets); i++ {
+		input_channels[current_worker] <- targets[i]
+		current_worker++
+		if current_worker == len(input_channels) {
+			current_worker = 0
+		}
+	}
+	//Close input channels.
+	for _,channel := range input_channels {
+		close(channel)
+	}
+	//Wait for output channels to return.
+	output := make(map[string]string)
+	for _,channel := range output_channels {
+		for {
+			val,ok := <-channel
+			if !ok { break }
+			output["unknown"] = val
+		}
+	}
+	fmt.Println(output)
+	
+	//needs a way to correlate output with targets...
 }
