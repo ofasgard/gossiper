02d8da384671132060b40a20940563dbcad86ff5
Callum Murphy <callum.a.murphy.77@gmail.com>
2021-08-23 12:21:57 +0100

    implement MapOptionsTCP()

--- src/siplib/map.go
+++ src/siplib/map.go
@@ -16,3 +16,20 @@ func MapOptionsUDP(target string, port int, timeout int, sender SIPRecipient, re
 	}
 	return "[NONE]",nil
 }
+
+// Single-threaded OPTIONS scan via TCP.
+
+func MapOptionsTCP(target string, port int, timeout int, sender SIPRecipient, receiver SIPRecipient) (string, error) {
+	req := NewOptionsRequest("TCP", sender, receiver)
+	res,err := RequestTCP(target, port, timeout, req)
+	if err != nil {
+		return "",err
+	}
+	if val,ok := res.Headers["Server"]; ok {
+		return val,nil
+	}
+	if val,ok := res.Headers["User-Agent"]; ok {
+		return val,nil
+	}
+	return "[NONE]",nil
+}
