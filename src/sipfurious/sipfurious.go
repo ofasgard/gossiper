58ff29abe5fd05db62f37b8b5690cd1b9faea169
Callum Murphy <callum.a.murphy.77@gmail.com>
2021-08-23 13:02:30 +0100

    minor updates

--- src/sipfurious/sipfurious.go
+++ src/sipfurious/sipfurious.go
@@ -5,6 +5,10 @@ import "time"
 import "math/rand"
 import "fmt"
 
+// For SIP implementation info see:
+// http://siptutorial.net/SIP/request.html
+// https://datatracker.ietf.org/doc/html/rfc3261
+
 func main() {
 	simpletest()
 }
@@ -20,5 +24,30 @@ func simpletest() {
 	fmt.Println(err)
 }
 
-// http://siptutorial.net/SIP/request.html
-// https://datatracker.ietf.org/doc/html/rfc3261
+func map_udp(targets []string, port int, timeout int) {
+	res_targets := []string{}
+	results := []string{}
+	for _,target := range targets {
+		fmt.Printf("Trying %s:%d...\n", target, port)
+		result,err := siplib.MapUDP(target, port, timeout)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not map %s:%d (%s)\n", target, port, err.Error())
+		} else {
+			res_targets = append(res_targets, target)
+			results = append(results, result)
+		}
+	}
+	fmt.Println("")
+	if len(res_targets) > 0 {
+		w := new(tabwriter.Writer)
+		w.Init(os.Stdout, 0, 8, 2, '\t', 0)
+		fmt.Fprintf(w, "Target\tPort\tServer Header\n")
+		fmt.Fprintf(w, "\t\t\t\n")
+		for index,_ := range res_targets {
+			fmt.Fprintf(w, "%s\t%d\t%s\n", res_targets[index], port, results[index])
+		}
+		w.Flush()
+	} else {
+		fmt.Println("No results found.")
+	}
+}
